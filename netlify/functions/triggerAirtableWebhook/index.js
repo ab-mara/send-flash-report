92f53e6bc5feaf624d6adf7949dfada271d6c610
rhinonix <1107968+rhinonix@users.noreply.github.com>
2023-07-28 17:49:07 +0200

    Initial commit

---
+++ netlify/functions/triggerAirtableWebhook/index.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  try {
+    const { recordID } = event.queryStringParameters;
+
+    if (!recordID) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Missing "recordID" parameter.' }),
+      };
+    }
+
+    const data = {
+      recordID: recordID,
+    };
+
+    const airtableWebhookURL = 'AT_WEBHOOK_URL_FLASH';
+
+    const response = await axios.post(airtableWebhookURL, data);
+
+    if (response.status === 200) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Webhook triggered successfully.' }),
+      };
+    } else {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ message: 'Failed to trigger webhook.' }),
+      };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error.' }),
+    };
+  }
+};
