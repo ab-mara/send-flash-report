ff8fbe970235eeae3ca92af020b92dde377b60e3
rhinonix <1107968+rhinonix@users.noreply.github.com>
2023-09-14 12:03:40 +0200

    Updated

---
+++ netlify/functions/rssParser/index.js
@@ -0,0 +1,35 @@
+const Parser = require("rss-parser");
+const parser = new Parser();
+
+exports.handler = async (event, context) => {
+  try {
+    const { queryStringParameters } = event;
+    const { feedUrl } = queryStringParameters;
+
+    if (!feedUrl) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: "Missing feedUrl parameter" }),
+      };
+    }
+
+    const feed = await parser.parseURL(feedUrl);
+    const jsonFeed = {
+      items: feed.items.map((item) => ({
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+      })),
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonFeed),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch or parse RSS feed" }),
+    };
+  }
+};
